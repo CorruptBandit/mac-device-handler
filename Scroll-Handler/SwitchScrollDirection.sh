26b21fdb158ddd4688755aeccf53db07d64f5cd0
Ollie Smith <ollie@Ollies-MacBook-Pro.local>
2023-07-30 23:28:23 +0100

    Cleanup the scroll handler pos args

--- Scroll-Handler/SwitchScrollDirection.sh
+++ Scroll-Handler/SwitchScrollDirection.sh
@@ -2,28 +2,22 @@
 
 set -e
 
-if [[ ! "${@}" ]];then
+if [[ ! "${@}" ]];
+then
     echo "ERROR: No scroll direction was given."
     exit -1
-fi
-
-if [[ "${1}" != "Natural" ]]
-then
-	direction="Standard"
 else
-	direction="Natural"
+	direction="${1}"
 fi
 
 osascript - "${direction}" >/dev/null << EOT
 	on run argv
-		set direction to item 1 of argv
+`		set direction to item 1 of argv
 		tell application "System Settings"
 			activate
 		end tell
-
 		tell application "System Events" to tell process "System Settings"
 			click menu item "Trackpad" of menu "View" of menu bar 1
-			delay 0.25
 			click radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 			set theCheckbox to checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 			if direction is equal to "Natural" then
@@ -36,6 +30,6 @@ osascript - "${direction}" >/dev/null << EOT
 				end tell
 			end if
 		end tell
-		quit application "System Settings"
+		quit application "System Settings"`
 	end run
 EOT
