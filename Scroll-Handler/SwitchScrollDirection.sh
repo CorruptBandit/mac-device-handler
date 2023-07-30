97736c56b72903143d8ad8f3b53ba5efb515845b
Ollie Smith <ollie@Ollies-MacBook-Pro.local>
2023-07-31 00:06:19 +0100

    Fix timings in the scroll handler

--- Scroll-Handler/SwitchScrollDirection.sh
+++ Scroll-Handler/SwitchScrollDirection.sh
@@ -12,14 +12,20 @@ fi
 
 osascript - "${direction}" >/dev/null << EOT
 	on run argv
-`		set direction to item 1 of argv
+		set direction to item 1 of argv
 		tell application "System Settings"
 			activate
 		end tell
 		tell application "System Events" to tell process "System Settings"
 			click menu item "Trackpad" of menu "View" of menu bar 1
+			repeat while not (exists radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1)
+				delay 0.1
+			end repeat
 			click radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 			set theCheckbox to checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
+			repeat while not (exists theCheckbox)
+				delay 0.1
+			end repeat
 			if direction is equal to "Natural" then
 				tell theCheckbox
 					if not (its value as boolean) then click theCheckbox
@@ -30,6 +36,6 @@ osascript - "${direction}" >/dev/null << EOT
 				end tell
 			end if
 		end tell
-		quit application "System Settings"`
+		quit application "System Settings"
 	end run
 EOT
