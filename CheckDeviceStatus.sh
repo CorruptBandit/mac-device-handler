628250415e312c6ca82ed64dded9f4b65d02405b
Ollie Smith <ollie@Ollies-MacBook-Pro.local>
2023-07-31 23:39:25 +0100

    Increase reliability for Scroll-Handler and stop CheckDeviceStatus false positives

--- CheckDeviceStatus.sh
+++ CheckDeviceStatus.sh
@@ -15,26 +15,34 @@ else
     DEVICE_TO_CHECK="${1}"
 fi
 
-change_settings() {
-    if [[ "$(ioreg -p IOUSB)" == *"${DEVICE_TO_CHECK}"* ]]
-        then
-            /usr/local/bin/SwitchKeyboard "British-PC"
-            /usr/local/bin/SwitchScrollDirection "Standard"
-        else
-            /usr/local/bin/SwitchKeyboard "British"
-            /usr/local/bin/SwitchScrollDirection "Natural"
-        fi
-            _tmp="$(ioreg -p IOUSB)"
+get_device_status() {
+    if [[ "$(ioreg -p IOUSB | grep -i "${DEVICE_TO_CHECK}")" ]]
+    then
+        echo "plugged"
+    else
+        echo "unplugged"
+    fi
 }
 
-_tmp="$(ioreg -p IOUSB)"
+change_settings() {
+    if [[ "$(get_device_status)" == "plugged" ]]
+    then
+        /usr/local/bin/SwitchKeyboard "British-PC"
+        /usr/local/bin/SwitchScrollDirection "Standard"
+    else
+        /usr/local/bin/SwitchKeyboard "British"
+        /usr/local/bin/SwitchScrollDirection "Natural"
+    fi
+}
 
+_prev_status="$(get_device_status)"
 change_settings
 while true
 do
-    if [[ "$(ioreg -p IOUSB)" != "${_tmp}" ]]
-    then 
+    if [[ "$(get_device_status)" != "${_prev_status}" ]]
+    then
+        _prev_status="$(get_device_status)"
         change_settings
     fi
-	sleep 3
+    sleep 3
 done
--- Scroll-Handler/SwitchScrollDirection.sh
+++ Scroll-Handler/SwitchScrollDirection.sh
@@ -13,37 +13,7 @@ fi
 osascript - "${direction}" >/dev/null << EOT
 	on run argv
 		set direction to item 1 of argv
-		set settings_pane to "Trackpad"
-		tell application "System Settings"
-			activate
-		end tell
-		tell application "System Events"
-			tell application process "System Settings"
-				tell splitter group 1 of group 1 of window 1
-					repeat until outline 1 of scroll area 1 of group 1 exists
-						delay 0
-					end repeat
-					tell outline 1 of scroll area 1 of group 1
-						set row_names to value of static text of UI element 1 of every row
-						repeat with i from 1 to (count row_names)
-							if settings_pane is not "Apple ID" then
-								if item i of row_names = {settings_pane} then
-									select row i
-									exit repeat
-								end if
-							else
-								try
-									if item 1 of item i of row_names contains settings_pane then
-										select row i
-										exit repeat
-									end if
-								end try
-							end if
-						end repeat
-					end tell
-				end tell
-			end tell
-		end tell
+		do shell script "open x-apple.systempreferences:com.apple.Trackpad-Settings.extension"
 		tell application "System Events" to tell process "System Settings"
 			repeat while not (exists radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1)
 				delay 0
