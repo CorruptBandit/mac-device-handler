1fc54f14feef6b390afbbb4b9b802e68ce67a2ce
Oliver Smith <90264544+CorruptBandit@users.noreply.github.com>
2023-08-10 20:45:48 +0100

    Update SwitchScrollDirection.sh
    Fix Ventura bug where the settings does not show the right screen

--- Scroll-Handler/SwitchScrollDirection.sh
+++ Scroll-Handler/SwitchScrollDirection.sh
@@ -18,11 +18,11 @@ osascript - "${direction}" >/dev/null << EOT
 			repeat while not (exists radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1)
 				delay 0
 			end repeat
-			click radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
-			set theCheckbox to checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
-			repeat while not (exists theCheckbox)
-				delay 0
+			repeat while not (exists checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1)
+				click radio button 1 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
+				click radio button 2 of tab group 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 			end repeat
+			set theCheckbox to checkbox "Natural scrolling" of group 1 of scroll area 1 of group 1 of group 2 of splitter group 1 of group 1 of window 1
 			if direction is equal to "Natural" then
 				tell theCheckbox
 					if not (its value as boolean) then click theCheckbox
